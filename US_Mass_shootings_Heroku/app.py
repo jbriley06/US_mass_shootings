0d16d8aeec93c1fa262a877915f1f791be73b3a3
Subhashini Kashinath <subhasushi@gmail.com>
2018-03-02 22:23:33 -0600

    adding final files

--- US_Mass_shootings_Heroku/app.py
+++ US_Mass_shootings_Heroku/app.py
@@ -1,22 +1,41 @@
 # Dependencies
-import pymongo
+from flask_pymongo import pymongo
 import datetime
+import os
 from collections import defaultdict
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, Markup
 from operator import itemgetter
+from bs4 import BeautifulSoup
+import requests
+
+MONGODB_URI = os.environ.get('MONGODB_URI')
+if not MONGODB_URI:
+   MONGODB_URI = "mongodb://localhost:27017/us_mass_shootings_2018"
+
+app = Flask(__name__)
+
+app.config["MONGO_URI"] = MONGODB_URI
 
 conn = 'mongodb://localhost:27017'
 client = pymongo.MongoClient(conn)
 
-db = client.us_mass_shootings
+db = client.us_mass_shootings_2018
 collection = db.incidents
 shootings = db.shootings
-# collection1 = db.gun_laws_by_states
 
-app = Flask(__name__)
+
 @app.route("/")
 def plots():
     return render_template("index.html")
+
+@app.route("/plots")
+def shootingsperyear():
+    return render_template("shootingsperyear.html")
+
+
+@app.route("/choropleth")
+def gunlaws():
+    return render_template("choropleth.html")
 
 @app.route("/incidents_per_year")
 def index():
@@ -70,7 +89,7 @@ def incidents():
         incident_dict = {
             "place": incident["LOCATION"],
             "state": incident["STATE"],
-            "year": incident["YEAR"],            
+            "date": incident["DATE"],            
             "name": incident["CASE"],
             "victims" : incident["TOTALVICTIMS"],
             "location": [incident["LATITUDE"], incident["LONGITUDE"]],
@@ -82,6 +101,48 @@ def incidents():
         incident_list.append(incident_dict)
 
     return jsonify(incident_list)
+
+
+@app.route("/states")
+def states():
+    return render_template("states.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/getlaws/<state>")
+def getlaws(state):
+
+    # URL of page to be scraped
+    base_url = "https://en.wikipedia.org/wiki/Gun_laws_in_"
+    url = base_url + state
+
+    # Retrieve page with the requests module
+    response = requests.get(url)
+    # Create BeautifulSoup object; parse with 'lxml'
+    soup = BeautifulSoup(response.text, 'lxml')
+    results = soup.find_all('table', class_='wikitable')
+    value = Markup(results[0])
+    return value
+
+@app.route("/venue")
+def shootingvenue():
+    events = list(shootings.find())
+
+    venue_list = []
+    for event in events:
+        if event["Venue"] != "":
+            temp = event["Venue"]
+            new = temp.replace("\n", "")
+            venue_list.append(new)
+
+    d = defaultdict(int)
+    for each in venue_list:
+        d[each] += 1  
+
+    return jsonify(d) 
 
 if __name__ == "__main__":
     app.run(debug=True)
